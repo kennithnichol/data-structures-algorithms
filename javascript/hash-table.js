b94c731ccfd86242013ced2588d2df81bb3be392
Kennith Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-11-09 08:32:23 -0800

    Crude hash table implementation in JavaScript

---
+++ javascript/hash-table.js
@@ -0,0 +1,42 @@
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+        return hash;
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key,value]);
+        return this.data;
+    }
+
+    get(key) {
+        const address = this._hash(key);
+        const bucket = this.data[address];
+        if (bucket) {
+            for(let i = 0; i < bucket.length; i++) {
+                if (bucket[i][0] === key) {
+                    return bucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+}
+
+// check
+const myHashTable = new HashTable(50);
+myHashTable.set('nova', 10000);
+myHashTable.get('nova');
+myHashTable.set('blackhole', 3);
+myHashTable.get('blackhole');
